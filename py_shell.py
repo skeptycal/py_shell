d8ad22f9cfa9376e12e0edfad9fabddaf9775cd9
skeptycal <skeptycal@gmail.com>
2019-04-10 12:49:29 -0500

    add default return value ; add module docstring

--- py_shell.py
+++ py_shell.py
@@ -1,23 +1,25 @@
 #!/usr/bin/env python3
 # -*- encoding: utf-8 -*-
-###############################################################################
-# py_shell.py 
-#
-# Returns: str -- python shell name. One of: ['IPython', 'IPython Notebook',
-#          'Jupyter Notebook', ‘CPython’, ‘IronPython’, ‘Jython’, ‘PyPy’]
-###############################################################################
+""" module py_shell.py
+
+    Returns: str -- python shell name. One of: ['IPython', 'IPython Notebook',
+         'Jupyter Notebook', ‘CPython’, ‘IronPython’, ‘Jython’, ‘PyPy’]
+    """
+
+
 def py_shell() -> str:
     """ Returns current python shell name.
-    
+
         Returns:
             str -- python shell name. One of: ['IPython', 'IPython Notebook',
             'Jupyter Notebook', ‘CPython’, ‘IronPython’, ‘Jython’, ‘PyPy’]
         """
     import os
+    from typing import Dict
 
-    PY_ENV: dict = os.environ
+    PY_ENV: Dict = os.environ
     PY_BASE: str = os.path.basename(PY_ENV['_'])
-    shell: str = "shell"
+    shell: str = "unknown"
 
     if "JPY_PARENT_PID" in PY_ENV:
         if "jupyter-notebook" in PY_BASE:
@@ -31,6 +33,7 @@ def py_shell() -> str:
         shell = platform.python_implementation()  # requires Python 2.6
     result = shell.strip()
     return result
+
 
 if __name__ == "__main__":
     print("The current python shell is: ", py_shell())
